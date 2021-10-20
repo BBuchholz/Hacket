400659e3f4db7b7e6a31f18c5fff0334260ec3d2
ENTOMDT <entomdt@gmail.com>
2021-10-20 13:39:57 -0500

    Add more scenarios

---
+++ tarot-cards-new.js
@@ -0,0 +1,150 @@
+// You can add new scenarios, but make sure that there is exactly...
+// * 1 daemonCard per scenario
+// * 3 playerCards per scenario
+
+var scenarios = [
+	
+	{
+		daemonCard: 
+		{
+			description: "Four of Disks",
+			power: "4D"
+		},
+		playerCards: [
+	{
+		description: "Five of Wands",
+		power: "5W"},
+	{
+		description: "Three of Cups",
+		power: "3C"},
+	{
+		description: "Two of Swords",
+		power: "2S"}]},
+	{
+		daemonCard: 
+	{
+		description: "Six of Disks",
+		power: "6D"
+	},
+		playerCards: [
+	{
+		description: "Five of Swords",
+		power: "5S"},
+	{
+		description: "Three of Disks",
+		power: "3D"},
+	{
+		description: "Two of Wands",
+		power: "2W"}]},
+	{
+		daemonCard: 
+	{
+		description: "Nine of Disks",
+		power: "9D"
+	},
+		playerCards: [
+	{
+		description: "Six of Wands",
+		power: "6W"},
+	{
+		description: "Eight of Cups",
+		power: "8C"},
+	{
+		description: "Four of Swords",
+		power: "4S"}]},
+	{
+		daemonCard: 
+	{
+		description: "Five of Cups",
+		power: "5C"
+	},
+		playerCards: [
+	{
+		description: "Eight of Disks",
+		power: "8D"},
+	{
+		description: "Three of Swords",
+		power: "3S"},
+	{
+		description: "Two of Disks",
+		power: "2D"}]},
+	{
+		daemonCard: 
+	{
+		description: "Seven of Disks",
+		power: "7D"
+	},
+		playerCards: [
+	{
+		description: "Five of Disks",
+		power: "5D"},
+	{
+		description: "Three of Wands",
+		power: "3W"},
+	{
+		description: "Ten of Swords",
+		power: "10S"}]},
+	{
+		daemonCard: 
+	{
+		description: "Seven of Wands",
+		power: "7W"
+	},
+		playerCards: [
+	{
+		description: "Eight of Wands",
+		power: "8W"},
+	{
+		description: "Six of Swords",
+		power: "6S"},
+	{
+		description: "Nine of Swords",
+		power: "9S"}]},
+	{
+		daemonCard: 
+	{
+		description: "Seven of Cups",
+		power: "7C"
+	},
+		playerCards: [
+	{
+		description: "Ten of Disks",
+		power: "10D"},
+	{
+		description: "Nine of Wands",
+		power: "9W"},
+	{
+		description: "Two of Cups",
+		power: "2C"}]},
+	{
+		daemonCard: 
+	{
+		description: "Ten of Wands",
+		power: "10W"
+	},
+		playerCards: [
+	{
+		description: "Ten of Cups",
+		power: "10C"},
+	{
+		description: "Nine of Cups",
+		power: "9C"},
+	{
+		description: "Four of Cups",
+		power: "4C"}]},
+	{
+		daemonCard: 
+	{
+		description: "Eight of Swords",
+		power: "8S"
+	},
+		playerCards: [
+	{
+		description: "Seven of Swords",
+		power: "7S"},
+	{
+		description: "Four of Wands",
+		power: "4W"},
+	{
+		description: "Six of Cups",
+		power: "6C"}]}]
